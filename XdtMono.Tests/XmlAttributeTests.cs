3c317e319189d39fc2ccadd8303f54d0b3de36ea
Matt Ward <ward.matt@gmail.com>
2014-04-30 18:29:29 +0100

    Add test for setting XmlAttribute.Prefix to null.

---
+++ XdtMono.Tests/XmlAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using System.Xml;
+
+namespace XdtMono.Tests
+{
+	[TestFixture]
+	public class XmlAttributeTests
+	{
+		[Test]
+		public void SetPrefixToNull ()
+		{
+			var doc = new XmlDocument ();
+			doc.LoadXml ("<root attr='' />");
+			XmlAttribute attribute = doc.DocumentElement.Attributes [0];
+
+			Assert.DoesNotThrow (() => attribute.Prefix = null);
+
+			Assert.AreEqual (String.Empty, attribute.Prefix);
+		}
+	}
+}
+
