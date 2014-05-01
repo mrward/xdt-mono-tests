97d5a8b1a729ca32cd202386873a86ddb6a25bd6
Matt Ward <ward.matt@gmail.com>
2014-05-01 10:14:52 +0100

    Add test for XmlNodeList.

---
+++ XdtMono.Tests/XmlNodeListTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+using NUnit.Framework;
+
+namespace XdtMono.Tests
+{
+	[TestFixture]
+	public class XmlNodeListTests
+	{
+		[Test]
+		public void RemoveNodeFromRootShouldNotChangeXmlNodeList ()
+		{
+			string xml = 
+@"<root>
+	<child id='1'/>
+	<child id='2'/>
+	<child id='3'/>
+</root>";
+			var doc = new XmlDocument ();
+			doc.LoadXml (xml);
+			XmlNodeList children = doc.SelectNodes ("//child");
+			XmlNode firstChild = children [0];
+			int childrenCountBeforeRemove = children.Count;
+
+			doc.DocumentElement.RemoveChild (firstChild);
+
+			Assert.AreEqual (3, children.Count);
+		}
+	}
+}
+
